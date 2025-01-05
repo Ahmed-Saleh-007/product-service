7c817cfa579527e3892962c354cf7edc556a0753
yousseftarek50 <youssef.tarek50@gmail.com>
2025-01-05 14:30:38 +0200

    add category repository

---
+++ src/main/java/com/ejada/product/service/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.ejada.product.service.repository;
+
+import com.ejada.product.service.model.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Integer> {
+}
