1ae89f5db7c4a3fffb44d97210ad228b2c60c1af
Fares3993 <84674642+Fares3993@users.noreply.github.com>
2025-01-06 12:59:57 +0200

    softDelet

--- src/main/java/com/ejada/product/service/controller/ProductController.java
+++ src/main/java/com/ejada/product/service/controller/ProductController.java
@@ -17,12 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -86,6 +81,11 @@ public class ProductController {
             @Valid @RequestBody CreateProductRequest request
     ) {
         return ResponseEntity.status(HttpStatus.OK).body(productService.createProduct(request));
+    }
+    @PutMapping("softDelete/{productId}")
+    public ResponseEntity<String> softDelete(@PathVariable int productId) {
+        productService.softDeleteProduct(productId);
+        return ResponseEntity.ok("Product soft deleted successfully."+productId);
     }
 
 }
