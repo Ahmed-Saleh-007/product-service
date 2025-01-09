7577884b126cac9042688ff1eda687ef5dc87cd8
Fares3993 <84674642+Fares3993@users.noreply.github.com>
2025-01-09 09:34:09 +0200

    Mapper-Annotations

--- src/main/java/com/ejada/product/service/model/response/OrderHistoryResponse.java
+++ src/main/java/com/ejada/product/service/model/response/OrderHistoryResponse.java
@@ -1,4 +1,6 @@
 package com.ejada.product.service.model.response;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -13,11 +15,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderHistoryResponse {
+    @Schema(description = "Customer Id", name = "CustomerId")
+    @JsonProperty("CustomerId")
     private int customerId;
+    @Schema(description = "Customer Name", name = "CustomerName")
+    @JsonProperty("CustomerName")
     private String customerName;
+    @Schema(description = "Order Id", name = "OredreId")
+    @JsonProperty("OredreId")
     private int orderId;
+    @Schema(description = "Order Date", name = "OrderDate")
+    @JsonProperty("OrderDate")
     private LocalDateTime orderDate;
+    @Schema(description = "Order Status", name = "OrderStatus")
+    @JsonProperty("OrderStatus")
     private String orderStatus;
+    @Schema(description = "Total Cost", name = "TotalCost")
+    @JsonProperty("TotalCost")
     private Double totalCost;
+    @Schema(description = "Product Details", name = "ProductDetails")
+    @JsonProperty("ProductDetails")
     private  List<ProductDetailsResponse> productDetails;
 }
--- src/main/java/com/ejada/product/service/model/response/ProductDetailsResponse.java
+++ src/main/java/com/ejada/product/service/model/response/ProductDetailsResponse.java
@@ -1,4 +1,6 @@
 package com.ejada.product.service.model.response;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +14,19 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDetailsResponse {
+    @Schema(description = "Product Name", name = "ProductName")
+    @JsonProperty("ProductName")
     private String productName;
+    @Schema(description = "Product Quantity", name = "Quantity")
+    @JsonProperty("Quantity")
     private Integer quantity;
+    @Schema(description = "Product Price", name = "Price")
+    @JsonProperty("Price")
     private BigDecimal price;
+    @Schema(description = "Total Cost Per Product Quantity", name = "Subtotal")
+    @JsonProperty("Subtotal")
     private BigDecimal subtotal;
+    @Schema(description = "Category Name", name = "CategoryName")
+    @JsonProperty("CategoryName")
     private String categoryName;
 }
